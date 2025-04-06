324c9b2d58e28a0af87ea56ba93b2a24327b06f3
Gabriel-Carvalho-ptbr <gabrielcoliveira1234@gmail.com>
2025-04-06 13:29:19 -0300

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-// Selecionando todos os itens de link dentro do menu
+
 const menuItems = document.querySelectorAll('#menu a');
 
-// Adiciona eventos apenas aos links individuais
+
 menuItems.forEach(item => {
-    // Alterar a cor de fundo ao passar o mouse sobre o link
+    
     item.addEventListener('mouseover', () => {
-        item.style.backgroundColor = 'rgba(0, 255, 135, 0.2)'; // Destaque apenas no link
+        item.style.backgroundColor = 'rgba(0, 255, 135, 0.2)'; 
     });
 
-    // Remove o fundo ao sair do link
+   
     item.addEventListener('mouseout', () => {
-        item.style.backgroundColor = ''; // Remove o destaque
+        item.style.backgroundColor = ''; 
     });
 
-    // Adiciona evento de clique
+   
     item.addEventListener('click', (event) => {
-        event.preventDefault(); // Evita comportamento padrão
+        event.preventDefault();
 
-        // Faz o direcionamento suave para a seção correspondente
+ 
         const targetSection = document.querySelector(item.getAttribute('href'));
         if (targetSection) {
             targetSection.scrollIntoView({ behavior: 'smooth' });
